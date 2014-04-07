01474222484694a321b474ee596e50477cfd0e8c
Mohamed Eltuhamy <tuhamy@gmail.com>
2014-04-07 14:57:33 +0100

    Greatly simplified C++ Instance layout

--- rpc/handcoded/src/logger/LoggerModule.cpp
+++ rpc/handcoded/src/logger/LoggerModule.cpp
@@ -1,30 +1,63 @@
-/*
- * LoggerModule.cpp
- *
- *  Created on: 12 Mar 2014
- *      Author: meltuhamy
- */
+// Instance
+#include "Logger.h" // this is what needs to be implemented by user.
+#include <ppapi/cpp/instance.h>
+#include "NaClRPCInstance.h"
+#include "ppapi/cpp/var.h"
+#include "ppapi/cpp/var_array.h"
+#include "ppapi/cpp/var_dictionary.h"
 
-#include "LoggerInstance.h"
-#include "LoggerModule.h"
+class LoggerInstance: public NaClRPCInstance {
+public:
+	LoggerInstance(PP_Instance instance) : NaClRPCInstance(instance) {}
+	virtual ~LoggerInstance(){}
 
-LoggerModule::LoggerModule() {
-	// TODO Auto-generated constructor stub
+	// Handle RPC method calls
+	virtual void HandleRPC(std::string name, pp::VarArray params, int id){
+		if(name == "helloName"){
+			// expecting 1 param with type string
+			if(params.GetLength() == 1){
+				pp::Var firstParam = params.Get(0);
+				if(firstParam.is_string()){
+					// this is the actual method call, implemented in Logger.cpp
+					std::string result = helloName(firstParam.AsString());
+					// Now do a callback
+					pp::Var responseData = pp::Var(result);
+					PostMessage(*ConstructResponseDictionary(id, responseData));
+				}
+			}
+		} else if(name == "greetName"){
+			// expecting 2 params with type string
+			if(params.GetLength() == 2){
+				pp::Var firstParam = params.Get(0);
+				pp::Var secondParam = params.Get(1);
+				if(firstParam.is_string() && secondParam.is_string()){
+					std::string result = greetName(firstParam.AsString(), secondParam.AsString());
+					// Now do a callback
+					pp::Var responseData = pp::Var(result);
+					PostMessage(*ConstructResponseDictionary(id, responseData));
+				}
+			}
+		}
+	}
 
-}
+};
 
-LoggerModule::~LoggerModule() {
-	// TODO Auto-generated destructor stub
-}
 
-pp::Instance* LoggerModule::CreateInstance(PP_Instance instance){
-	return new LoggerInstance(instance);
-}
 
+// Module
+#include <ppapi/cpp/module.h>
+
+class LoggerModule: public pp::Module {
+public:
+	LoggerModule() : pp::Module() {}
+	virtual ~LoggerModule() {}
+	virtual pp::Instance* CreateInstance(PP_Instance instance) {
+		return new LoggerInstance(instance);
+	}
+};
 
 namespace pp {
-// Every module must implement a factory method
-Module* CreateModule() {
-  return new LoggerModule();
+	Module* CreateModule() {
+	  return new LoggerModule();
+	}
 }
-}  // namespace pp
