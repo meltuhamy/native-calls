f0dae520a3bce7a320a5491d49f0732822a05967
Mohamed Eltuhamy <tuhamy@gmail.com>
2014-05-11 14:19:53 +0100

    Better default context checking

--- scripts/NaClConfig.js
+++ scripts/NaClConfig.js
@@ -1,8 +1,30 @@
 define([], function(){
   // allow global setting
-  return window.NaClConfig ? window.NaClConfig : {
+  var defaultConfig = {
     CONFIG: 'Debug',
     TOOLCHAIN: 'newlib',
     VALIDATION: true
+  };
+
+  if(window.NaClConfig){
+    var correctConfig = {};
+    for(var key in defaultConfig){
+      if(defaultConfig.hasOwnProperty(key)){
+        if(window.NaClConfig[key] != undefined){
+          correctConfig[key] = window.NaClConfig[key];
+        } else {
+          correctConfig[key] = defaultConfig[key];
+        }
+      }
+    }
+
+    return correctConfig;
+
+  } else {
+
+    return defaultConfig
+
   }
+
+
 });
