73da9417470301c3ac3da474a16347178bc9a8d9
Mohamed Eltuhamy <tuhamy@gmail.com>
2014-06-14 07:17:03 +0100

    Fix up bullet physics demo

--- demos/BulletRPC/data/stats.js
+++ demos/BulletRPC/data/stats.js
@@ -6,18 +6,29 @@ var NCStats = {
 	num_trips: 0,
 	interval: 1000
 };
+var intervalID = undefined;
 
 window.startPublishingStats = function(channel){
-	setInterval(function(){
-		// publish average every second
-		NCStats.publishTime = (new Date()).getTime();
-		$.jStorage.publish(channel, NCStats);
+  if(!intervalID){
+    intervalID = setInterval(function(){
+      // publish average every second
+      NCStats.publishTime = (new Date()).getTime();
+      $.jStorage.publish(channel, NCStats);
 
-		// reset
-		NCStats.frames = 0;
-		NCStats.total_simTime = 0;
-		NCStats.num_sims = 0;
-		NCStats.total_tripTime = 0;
-		NCStats.num_trips = 0;
-	}, NCStats.interval);
-}
+      // reset
+      NCStats.frames = 0;
+      NCStats.total_simTime = 0;
+      NCStats.num_sims = 0;
+      NCStats.total_tripTime = 0;
+      NCStats.num_trips = 0;
+    }, NCStats.interval);
+  }
+
+};
+
+window.stopPublishingStats = function(){
+  if(intervalID){
+    clearInterval(intervalID);
+    intervalID = undefined;
+  }
+};
